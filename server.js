d742a52167344486ae36f442c275f12c062bebb0
pastorjuradotraverso <pastor.methods@gmail.com>
2016-04-19 15:13:58 +0100

    Remove express

--- server.js
+++ server.js
@@ -1,12 +1,53 @@
-var http 		= require('http'),	
-    express		= require('express'),
-    app			= express(),
-    fs         	= require('fs'),
-    path 		= require("path"),
-    port = process.env.PORT || 3000; // set the port
+// var http 		= require('http'),
+//     express		= require('express'),
+//     app			= express(),
+//     fs         	= require('fs'),
+//     path 		= require("path"),
+//     port = process.env.PORT || 3000; // set the port
+//
+// app.use(express.static('build'));
+//
+// app.listen(port, function() {
+//     console.log('listening');
+// });
 
-app.use(express.static('build'));
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs"),
+    port = process.env.PORT || 3000;
 
-app.listen(port, function() {
-    console.log('listening');
-});
+
+http.createServer(function(request, response) {
+
+    var uri = url.parse(request.url).pathname
+        , filename = path.join(process.cwd(),"/build/" + uri );
+
+    console.log(filename);
+
+    fs.exists(filename, function(exists) {
+        if(!exists) {
+            response.writeHead(404, {"Content-Type": "text/plain"});
+            response.write("404 Not Found\n");
+            response.end();
+            return;
+        }
+
+        if (fs.statSync(filename).isDirectory()) filename += 'index.html';
+
+        fs.readFile(filename, "binary", function(err, file) {
+            if(err) {
+                response.writeHead(500, {"Content-Type": "text/plain"});
+                response.write(err + "\n");
+                response.end();
+                return;
+            }
+
+            response.writeHead(200);
+            response.write(file, "binary");
+            response.end();
+        });
+    });
+}).listen(parseInt(port, 10));
+
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
